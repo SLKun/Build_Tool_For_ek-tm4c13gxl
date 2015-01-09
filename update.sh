651b3f6b443eeb330b8c3b9e29528494803ebbe8
SL <Summerslyb@gmail.com>
2015-01-10 01:55:17 +0800

    V0.31 ADD install.sh

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf stable.zip Stable
-wget https://github.com/Summerslyb/Build_Tool_For_ek-tm4c13gxl/archive/stable.zip
-7z x stable.zip
-rm -rf stable.zip
-mv Build_Tool_For_ek-tm4c13gxl-stable Stable
-chmod +x Stable/*.sh
