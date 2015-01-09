9556d5bf9842efa1a93e54ab30c8028a6749c28f
SL <Summerslyb@gmail.com>
2015-01-09 20:02:08 +0800

    Just for Learning Cache

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ wget https://github.com/Summerslyb/Build_Tool_For_ek-tm4c13gxl/archive/stable.zi
 7z x stable.zip
 rm -rf stable.zip
 mv Build_Tool_For_ek-tm4c13gxl-stable Stable
+chmod +x Stable/*.sh
